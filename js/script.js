576e302ffeb6b9f2f3c142021d5d1190882f3157
Gaurav Gupta <alwar.gaurav1@gmail.com>
2022-11-27 20:33:22 +0530

    Done😎

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ======== Typing Animation =========== */
+var typed = new Typed(".typing",{
+    strings: ["","Web Developer", "Web Designer","Programmer" ],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
